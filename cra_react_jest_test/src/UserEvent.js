fa7a461246d42c9e8784a4c762804ef8ecdecfd9
JustWaveThings <justwavethings@proton.me>
2023-05-30 22:27:05 -0400

    Test user event

---
+++ cra_react_jest_test/src/UserEvent.js
@@ -0,0 +1,18 @@
+import React, {useState} from 'react'
+
+function UserEvent() {
+  const [heading, setHeading] = useState("Test Jest")
+
+  const clickHandler = () => {
+    setHeading("Not Testing Jest")
+  }
+
+  return (
+    <>
+      <button type='button' onClick={clickHandler}>Click me</button>
+      <h1>{heading}</h1>
+    </>
+  )
+}
+
+export default UserEvent
---
+++ cra_react_jest_test/src/UserEvent.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import UserEvent from "./UserEvent";
+
+describe("App component", () => {
+  it('renders test jest heading', () => {
+    const {container} = render(<UserEvent />)
+    expect(container).toMatchSnapshot()
+    })
+  it('renders "not testing jest" heading when button is clicked', async () => {
+      const user = userEvent.setup();
+
+      render(<UserEvent />);
+      const button = screen.getByRole("button", {name: /click me/i});
+
+      await user.click(button);
+
+      expect(screen.getByRole('heading').textContent).toMatch(/Not Testing Jest/i)
+})})
