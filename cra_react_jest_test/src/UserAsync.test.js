5fae6a7add8d62f46a9af9d4da88235f30b9325d
JustWaveThings <justwavethings@proton.me>
2023-06-06 21:43:41 -0400

    Error message if api down test pass

--- cra_react_jest_test/src/UserAsync.test.js
+++ cra_react_jest_test/src/UserAsync.test.js
@@ -7,8 +7,22 @@ import {
 import UserApp from "./UserApp";
 import "@testing-library/jest-dom";
 
-describe("academind testing async user", () => {
+describe("academind testing async user display, loading, and error display", () => {
   test("loading text is shown while API request is in progress", async () => {
+    window.fetch = jest.fn(() => {
+      const user = { name: "Jack", email: "jack@email.com" };
+      return Promise.resolve({
+        json: () => Promise.resolve(user),
+      });
+    });
+
+    render(<UserApp />);
+    const loading = screen.getByText("Loading...");
+
+    expect(loading).toBeInTheDocument();
+    await waitForElementToBeRemoved(() => screen.queryAllByText("Loading..."));
+  });
+  test("user name is rendered", async () => {
     window.fetch = jest.fn(() => {
       const user = { name: "Jack", email: "jack@email.com" };
 
@@ -16,9 +30,19 @@ describe("academind testing async user", () => {
         json: () => Promise.resolve(user),
       });
     });
+
     render(<UserApp />);
-    const loading = screen.getByText("Loading...");
-    expect(loading).toBeInTheDocument();
-    await waitForElementToBeRemoved(() => screen.queryAllByText("Loading..."));
+    const userName = await screen.findByText("Jack");
+
+    expect(userName).toBeInTheDocument();
   });
+  test("Error message is shown", async () => {
+    window.fetch.mockImplementationOnce(() => {
+      return Promise.reject({message: 'The API is down.'})
+    })
+
+    render(<UserApp/>)
+    const errorMessage = await screen.findByText('The API is down.')
+    expect(errorMessage).toBeInTheDocument();
+  })
 });
